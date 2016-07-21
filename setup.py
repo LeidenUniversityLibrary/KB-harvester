280f7ebc715abfaa2a87243016eee654afd298b6
bencomp <ben@companjen.name>
2016-07-21 17:58:09 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kbharvester',
-    version='0.1.0',
+    version='0.2.0',
     packages=['nl', 'nl.leidenuniv', 'nl.leidenuniv.library', 'nl.leidenuniv.library.harvester'],
     url='https://github.com/bencomp/KB-harvester',
     license='GPL',
